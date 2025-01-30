c701f51e9135a8f9713c3a42f16a99a3233199a6
Rob Gilbert <fox015@gmail.com>
2025-01-30 15:47:21 +1000

    Update base value

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   site: "https://fox015.github.io",
-  base: "fox015.github.io",
+  base: "astro-nano",
   integrations: [mdx(), sitemap(), tailwind()],
 });
